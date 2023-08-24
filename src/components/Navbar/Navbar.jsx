c77dc01829e3b9d5a1bac764e13817419206ec95
AleMMb <alejandramellab@gmail.com>
2023-08-23 20:13:21 -0400

    tailwind intall

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+export const Navbar = () => {
+  return (
+    <div>
+        <h1 className="text-3xl uppercase font-bold text-orange-800 bg-black">Hello world!</h1>
+        <h3>The best programer in the world!</h3>
+    </div>
+  )
+}
